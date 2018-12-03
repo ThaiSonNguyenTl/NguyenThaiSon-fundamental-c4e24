f97aab87c43e6381140fc1d4c3d6da365c71804c
CHÀO ANH SƠN Ạ <thaisonnguyentl@gmail.com>
2018-12-03 22:42:52 +0700

    initial commit

--- session2/homework2/serious_ex4_d.py
+++ session2/homework2/serious_ex4_d.py
@@ -1,5 +1,5 @@
 n = int(input("enter a number:"))
-for i in range(1, n ,2):
+for i in range(1, n ,):
     print("x", end=" ")
     print("*", end=" ")
 print("x", end = " ")
--- session3/list_intro.py
+++ session3/list_intro.py
@@ -6,7 +6,15 @@
 # print(items)
 
 items = ["bun dau", "bun bo ", "bun rieu"]
-print(items[-2])
+print(items)
+items.remove("bun bo")
+# items.pop(1)  #xoa vi tri so 1
+print(items)
+# print(items[1])
+# items[1]= "bun bo nam bo"
+# print(items[1])
+# print(items)
+#khi truy cap den dau thi coi la 1 bien
 # print(items)
 # items.append("thit duc ")
 # print(items)
