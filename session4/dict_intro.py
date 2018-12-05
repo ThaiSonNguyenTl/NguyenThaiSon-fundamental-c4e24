83ecfb16151b2086818963900b9838a1dc74c180
CHÀO ANH SƠN Ạ <thaisonnguyentl@gmail.com>
2018-12-06 02:10:02 +0700

    initial commit

--- session4/dict_intro.py
+++ session4/dict_intro.py
@@ -12,8 +12,17 @@ person = {
 "city": "Hai Phong",
 "age" : 25
 }
-print(person["name"])
-if "status" in person:  #check xem mot thu co nam trong mot thu khac hay ko if "..." in .....
-    print("yes")
-else:
-    print("no")
+person["age"] = 18
+print(person["age"])
+print (person)
+# if "status" in person:  #check xem mot thu co nam trong mot thu khac hay ko if "..." in .....
+#     print("yes")
+# else:
+#     print("no")
+person["age"] += 1
+print(person ["age"])
+person["status"] = False  # 
+print(person)
+#xoa
+# del person["city"]
+# print(person)
--- session4/ex_dict.py
+++ session4/ex_dict.py
@@ -4,6 +4,20 @@ dienthoai = {
 "opp": "oppo",
 "hw": "huaweir",
 }
-print(*dienthoai)
-code = str(input("your code? "))
-print(dienthoai[code])
+while True:
+    print(*dienthoai)
+    code = str(input("your code? "))
+    print(dienthoai[code])
+    print()
+    update = input("do you want to update(Y/N): ").upper()
+    if update == "Y":
+        new_translations = input("your translation:  ")
+        dienthoai[code] = new_translations
+        print("done")
+    else:
+        print("not found")
+        creat = input("Do you want to contribute(Y/N)?  ").upper()
+        if creat == "Y":
+            translation = input("your translation: ")
+            dienthoai[code] = translation
+        
